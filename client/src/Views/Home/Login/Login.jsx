f087d760b7adc7be2c4b7ba208621f645d1f0c57
DBrange <asesincreedaltairr@hotmail.com>
2023-03-15 04:26:22 -0300

    rename link login

--- client/src/Views/Home/Login/Login.jsx
+++ client/src/Views/Home/Login/Login.jsx
@@ -1,18 +1,14 @@
 import { Link } from "react-router-dom";
-import Register from "./Register";
 import Button from "../../../Components/Button";
 
 const Login = () => {
   return (
     <div>
-      <Link to="home/login/resgister">
-        <Button>
-        <Register />
-
-        </Button>
+      <Link to="/home/login/register">
+        <Button>Register</Button>
       </Link>
     </div>
   );
-}
+};
 
 export default Login;
