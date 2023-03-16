d7bd7aab16433a5dc06ab80521895383fa342b0d
DBrange <asesincreedaltairr@hotmail.com>
2023-03-16 16:36:56 -0300

    agregando index

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,12 @@ import Notifications from "./Views/Notifications/Notifications";
 import Organization from "./Views/Organization";
 import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 import SideBar from "./Components/SideBar/SideBar";
+import Payment from "./Views/Payment/Payment";
+import AddEmployee from "./Views/Employees/AddEmployee/AddEmployee";
 
 function App() {
   // const [user, setUser] = useState(null);
-  const {pathname} = useLocation()
+  const { pathname } = useLocation();
   const [user, setUser] = useState({
     name: "juan",
     role: ["admin"],
@@ -35,6 +37,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/home/login" element={<Login />} />
         <Route path="/home/login/register" element={<Register />} />
+        <Route path="/home/login/register/payment" element={<Payment />} />
         <Route element={<ProtectedRoute isAllowed={!!user} />}>
           <Route
             element={
@@ -46,7 +49,8 @@ function App() {
           >
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/employees" element={<Employees />} />
-            <Route path="/employees/:employeeId" element={<Employee />} />
+            <Route path="/employees/:id" element={<EmployeeDetail />} />
+            <Route path="/addemployee" element={<AddEmployee />} />
             <Route path="/organization" element={<Organization />} />
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/notifications" element={<Notifications />} />
