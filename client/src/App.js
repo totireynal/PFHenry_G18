43034329ab2629bc7e74584ec406178c761dbf6b
nottotem <totemstu@gmail.com>
2023-03-16 15:36:59 -0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,10 @@ import "./App.css";
 import Dashboard from "./Views/Dashboard";
 import Employees from "./Views/Employees";
 // import Employee from "./Views/Employees/Employee";
-import EmployeeDetail from "./Views/Employees/EmployeeDetail/EmployeeDetail";
 import EmployeeDetail from "./Views/EmployeeDetail/EmployeeDetail";
 import Home from "./Views/Home";
 import Login from "./Views/Home/Login";
 // import Register from "./Components/Register/Register.jsx"
-import Register from "./Components/Register/Register.jsx";
-import Payment from "./Components/Payment/Payment.jsx";
 import AddEmployee from "./Views/Employees/AddEmployee/AddEmployee";
 
 function App() {
@@ -25,7 +22,6 @@ function App() {
         <Route path="/employees" element={<Employees />} />
         <Route path="/addemployee" element={<AddEmployee />} />
         <Route path="/employee/:id" element={<EmployeeDetail />} />
-        {/* <Route path="/employees/:employeeId" element={<Employee />} /> */}
         <Route path="*" element={<h1>Ruta equivocada</h1>} />
       </Routes>
     </div>
