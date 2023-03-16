df903ae8bc9e4d6d6f6dcd6967a6c87398ef0476
Pablo Cogno <pablocogno.5@gmail.com>
2023-03-16 12:52:40 -0300

    Armado del componente Payment

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,29 @@ import Employees from "./Views/Employees";
 import Employee from "./Views/Employees/Employee";
 import Home from "./Views/Home";
 import Login from "./Views/Home/Login";
+<<<<<<< HEAD
 // import Register from "./Components/Register/Register.jsx"
+=======
+import Register from "./Components/Register/Register.jsx"
+import Payment from "./Components/Payment/Payment.jsx"
+>>>>>>> 7050622 (Armado del componente Payment)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       {/* <Register></Register> */}
       <Routes>
+=======
+      <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/home/login" element={<Login />} />
+    </Routes>
+
+      {/* <Register></Register> */}
+      {/* <Routes>
+>>>>>>> 7050622 (Armado del componente Payment)
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/home/login" element={<Login />} />
@@ -20,7 +36,11 @@ function App() {
         <Route path="/employees" element={<Employees />} />
         <Route path="/employees/:employeeId" element={<Employee />} />
         <Route path="*" element={<h1>Ruta equivocada</h1>} /> 
+<<<<<<< HEAD
       </Routes>
+=======
+      </Routes>  */}
+>>>>>>> 7050622 (Armado del componente Payment)
     </div>
   );
 }
