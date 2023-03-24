d5d6ea233688bf3f76e29a4dca88f693378c42cc
nottotem <totemstu@gmail.com>
2023-03-23 22:48:01 -0300

    new form email & detail

--- client/src/Views/Notifications/Notifications.jsx
+++ client/src/Views/Notifications/Notifications.jsx
@@ -1,9 +1,11 @@
-import MailForm from "../../Views/Notifications/Mail/MailForm";
+// import MailForm from "../../Views/Notifications/Mail/MailForm";
+import FormEmail from "../../Views/Notifications/Mail/FormEmail";
 
 const Notifications = () => {
   return (
-    <div className=" w-full h-screen p-8 ml-72">
-      <MailForm />
+    <div className=" w-full h-screen ml-72">
+      {/* <MailForm /> */}
+      <FormEmail />
     </div>
   );
 };
