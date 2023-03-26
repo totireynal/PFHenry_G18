82fbfef0417c7939d5357e1793ac164a9cb41ca6
totireynal <sofiareynalm@gmail.com>
2023-03-26 17:13:49 -0300

    dominio de Carlos

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import { CookiesProvider } from "react-cookie";
 
 import { Auth0Provider } from '@auth0/auth0-react'
 
-const domain = 'https://dev-zb5ab7mg5ollsy01.us.auth0.com';
-const clientId = 'yEXJ9J7A6wYtioS9Y8E1p535jZnzxmYR';
+const domain = 'dev-a5lp6h1utxb70h27.us.auth0.com';
+const clientId = 'KzGcYaMK0yVq39wFL8WaAH8BjmQ7yqlj';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
