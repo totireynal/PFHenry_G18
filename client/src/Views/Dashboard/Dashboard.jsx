6aa90cda93c2a40160bc354a23e3a5020b00e8ea
nottotem <totemstu@gmail.com>
2023-03-30 17:22:36 -0300

    crud a&p commentado

--- client/src/Views/Dashboard/Dashboard.jsx
+++ client/src/Views/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
-import AreaForm from "../../Components/AreaForm/AreaForm";
-import PositionForm from "../../Components/PositionForm/PositionForm";
+// import AreaForm from "../../Components/AreaForm/AreaForm";
+// import PositionForm from "../../Components/PositionForm/PositionForm";
 
 const Dashboarts = () => {
   return (
     <div className="w-full h-screen ml-72">
-      {/* <span>View Dashboard</span> */}
+      <span>View Dashboard</span>
       <div className="flex flex-row gap-6 items-center justify-center">
-        <AreaForm />
-        <PositionForm />
+        {/* <AreaForm /> */}
+        {/* <PositionForm /> */}
       </div>
     </div>
   );
