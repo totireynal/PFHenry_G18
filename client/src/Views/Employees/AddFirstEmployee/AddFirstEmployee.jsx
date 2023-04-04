9c13caa620065d265f37ce13c71ad6e27723b8f4
Pablo Cogno <pablocogno.5@gmail.com>
2023-04-04 18:59:40 -0300

    actualizacion develop

--- client/src/Views/Employees/AddFirstEmployee/AddFirstEmployee.jsx
+++ client/src/Views/Employees/AddFirstEmployee/AddFirstEmployee.jsx
@@ -7,9 +7,9 @@ import {
   getAllEmployees,
 } from "../../../state/redux/actions/actions";
 import FormFirstEmployee from "../../../Components/Form/FormFirstEmployee";
-import validate from "../../../utils/functions/validate";
-import { useErrors } from "../../../utils/hooks/errors";
-import { useAnswer } from "../../../utils/hooks/answer";
+import validate from "../../../Utils/functions/validate";
+import { useErrors } from "../../../Utils/hooks/errors";
+import { useAnswer } from "../../../Utils/hooks/answer";
 // import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
